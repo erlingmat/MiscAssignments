0b819c3f9dc33fc3d7129e0caef50b49766d9034
Erling Matthiesen Møller <erling.moeller@oneprediction.ai>
2020-09-24 21:38:50 +0200

    almost initial commit

---
+++ SkuAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkuAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
